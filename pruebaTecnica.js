f1f1bb934c1657dcc5313de4f797aa88df2e0ef3
AlexisPerdomoD <dropdibemol@gmail.com>
2023-11-12 23:17:12 -0500

    ajustes esteticos

--- pruebaTecnica.js
+++ pruebaTecnica.js
@@ -1,23 +1,18 @@
 let notas =[];
-
 let crear = document.getElementById("botonCrear");
 let contenidoNota = document.getElementById("textarea");
 let contenedorPrincipal = document.getElementById("contenedorPrincipal");
 
 crear.addEventListener("click", () => crearNota());
-  
-
-
-
 
 const crearNota = () => {
   if(contenidoNota.value){
-    notas = notas.filter(e => e !== "");
-    notas.push(contenidoNota.value);
-
+    notas = notas.filter(e => e !==  null)
+    notas.push(contenidoNota.value)
+    
     if(document.getElementById("notasContainer")){contenedorPrincipal.removeChild(document.getElementById("notasContainer"))}
-
     let notasContainer = document.createElement("div");notasContainer.setAttribute("id","notasContainer")
+
     let id = 0; 
     notas.forEach((e)=>{
       notasContainer.innerHTML +=`
@@ -25,15 +20,15 @@ const crearNota = () => {
         <p>${e}</p>
         <button value ="borrar" onclick=borrarNota(event.target)>Borrar</button>
       </div>`
-    });
-    contenedorPrincipal.appendChild(notasContainer);
-    contenidoNota.value = null;
+    })
+    contenedorPrincipal.appendChild(notasContainer)
+    contenidoNota.value = null
   }else{
-    alert("ingresa un valor")
+    alert("debe escribir algo para crear la nota")
   }
-}
+};
 const borrarNota = (boton) => {
-  let container = boton.closest(".container");
-  notas.splice(container.id, 1 ,"");
-  container.remove();
-}
+  let container = boton.closest(".container")
+  notas.splice(container.id, 1 ,null)
+  container.remove()
+};
